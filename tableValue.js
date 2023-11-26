30c8c854d88463d2b35fb31c5b6978af56d80609
iPoxzz <151027114+iPoxzz@users.noreply.github.com>
2023-11-27 01:36:11 +0400

    TableValues

---
+++ tableValue.js
@@ -0,0 +1,50 @@
+
+
+
+
+function ACE() {
+    this.ACE = "ACE";
+    this.price = 15.00;
+    this.image = "img/ACE.jpg";
+}
+
+function Shipping() {
+     this.urgent = 10.00;
+     this.normal = 5.00;
+}
+
+
+
+
+
+        const ace= new ACE()
+             let array1 = [
+    
+                 this.ACE ,
+                 this.price , 
+                 this.image ,
+    
+    
+                         ];
+
+        const shiping= new Shipping()
+                let array2 = [
+      
+                this.urgent,
+                this.normal,
+    
+                             ];
+    
+ 
+
+
+function addCart() {
+    let addCart = document.getElementById('newElementInput').value;
+    
+}
+
+
+function search() {
+    let searchTerm = document.getElementById('searchInput').value;
+    
+}
